cfe99082737d3a48fb656f97785e2002895c1404
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-01-17 16:25:36 -0600

    refactoring showTime()

--- script.js
+++ script.js
@@ -1,63 +1,47 @@
-// set container divs as variables
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
-const amPm = document.querySelector('.ampm');
-
-// amPm = n => {
-// 	if(n < 12) {
-// 		return "am";
-// 	}
-// 	return "pm";
-// }
-
-addZero = n => {
-	if(n <= 9) {
-		 return "0" + n;
- 	}
-	return n;
-}
-
-// get current time
-
 showTime = () => {
+	// set container divs as variables
+	const hours = document.querySelector('.hours');
+	const minutes = document.querySelector('.minutes');
+	const seconds = document.querySelector('.seconds');
+	const amPm = document.querySelector('.ampm');
+	const userHr = document.querySelector('.userHr');
+	const userMin = document.querySelector('.userMin');
+	const userSecs = document.querySelector('.userSecs');
+
+	// get current time
 	const now = new Date();
 	const secs = now.getSeconds();
 	const mins = now.getMinutes();
 	const hrs = now.getHours();
 	const ampm = hrs < 12 ? 'am' : 'pm'; 
+
+	// amPm = n => {
+	// 	if(n < 12) {
+	// 		return "am";
+	// 	}
+	// 	return "pm";
+	// }
+
+	addZero = n => n <= 9 ? "0" + n : n;
+
 	// display current time in divs
-	seconds.innerText = addZero(secs);
-	minutes.innerText = addZero(mins);
-	hours.innerText = addZero(hrs % 12 || 12);
-	amPm.innerText = ampm;
+	seconds.innerHTML = addZero(secs);
+	minutes.innerHTML = addZero(mins);
+	hours.innerHTML = addZero(hrs % 12 || 12);
+	amPm.innerHTML = ampm;
 	
-	// console.log(hours.innerText + ":" + minutes.innerText + ":" + seconds.innerText + " " + ampm);
+	console.log(hours.innerHTML + ":" + minutes.innerHTML + ":" + seconds.innerHTML + " " + ampm);
+		
 }
 
-const userHr = document.querySelector('.userHr');
-const userMin = document.querySelector('.userMin');
-const userSecs = document.querySelector('.userSecs');
-
-// function alarm() {
-// 	const now = new Date();
-// 	const secs = now.getSeconds();
-// 	const mins = now.getMinutes();
-// 	const hrs = now.getHours();
-	
-// 	if(hrs == userHr.value && mins == userMin.value && secs == userSecs.value) {
-// 		alert('time!');
-// 		console.log('time');
-// 	}
-	
-// 	console.log('alarm set' + userHr.value +
-// userMin.value + 
-// userSecs.value);
+// alarm = () => {
+// 	// if(seconds.innerText === "00" || seconds.innerText === "10" || seconds.innerText === "20" || seconds.innerText === "30" || seconds.innerText === "40" || seconds.innerText === "50") {
+// 		console.log('alarm');
+// 	// }
 // }
-
 // const alarmBtn = document.querySelector('#set-alarm');
 // alarmBtn.addEventListener('click', alarm);
 
-// setInterval(alarm, 1000);
+
 setInterval(showTime, 1000);
 
